55961498032252904fe92684e941a5af36571b3d
Nandini S <68180177+NANDINIS123@users.noreply.github.com>
2021-11-30 14:04:11 +0530

    Add files via upload

---
+++ flight day-1 demo case study/search_flight/src/main/java/Flight_Booking/search_flight_controller.java
@@ -0,0 +1,13 @@
+package Flight_Booking;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search_flights")
+public class search_flight_controller {
+@RequestMapping("/hey")
+public String hey() {
+	return "welcome to flight search";
+}
+}
