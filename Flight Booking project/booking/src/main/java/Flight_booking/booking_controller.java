f4431bec51a3fd5eb8df0ed09ae4e91f796b5828
Nandini S <68180177+NANDINIS123@users.noreply.github.com>
2021-11-30 12:49:57 +0530

    Add files via upload

---
+++ Flight Booking project/booking/src/main/java/Flight_booking/booking_controller.java
@@ -0,0 +1,13 @@
+package Flight_booking;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class booking_controller {
+@RequestMapping("/hey")
+public String hey() {
+	return "welcome to booking";
+}
+}
